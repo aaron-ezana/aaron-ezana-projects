933426664373124528befc050404dfd36e53e564
Aaron Ezana <aaron.ezana@gmail.com>
2025-10-02 16:12:17 +0100

    Add tuple example

---
+++ python-basics-course/tuple.py
@@ -0,0 +1,6 @@
+my_tuple = ("apple", "banana", "cherry")
+print(my_tuple)
+print(my_tuple[1])  # Accessing the second element
+#my_tuple[1] = "blueberry"  # This will raise an error because tuples are immutable
+x = (5)      # just a number
+y = (5,)     # this is a tuple
